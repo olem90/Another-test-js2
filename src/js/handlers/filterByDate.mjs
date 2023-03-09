5045d57951a4edc0a5bb5b552caeff61ab835562
olem90 <olemariusrognan90@gmail.com>
2023-03-09 01:16:14 +0100

    added sort posts by date filter

--- src/js/handlers/filterByDate.mjs
+++ src/js/handlers/filterByDate.mjs
@@ -1,38 +1,56 @@
 import { getPosts } from "../api/posts/read.mjs";
 import * as templates from "../templates/index.mjs"
 
+const path = location.pathname;
+
 // Filter posts by date with drop down menu
-const profilePostsContainer = document.querySelector("#homePosts")
+const homePostsContainer = document.querySelector("#homePosts");
 const SortPostsByNewest = document.querySelector(".newDate");
 const SortPostsByOldest = document.querySelector(".oldDate");
 
-export async function sortPostsByDate(event) {
+/**
+ * This function will sort posts from oldest to newest
+ * 
+ * @param {string} event posts sorted from oldest to newest
+ */
+export async function sortPostsByOldest(event) {
     event.preventDefault();
     const posts = await getPosts();
     const sortedFromOldest = posts.sort(byOldest);
-    const sortedFromNewest = posts.sort(byNewest);
-    console.log(sortedFromOldest)
-    if(sortedFromOldest){
-        
-        profilePostsContainer.innerHTML = "";    
-        templates.renderPostTemplates( sortedFromOldest, profilePostsContainer )
-    } else if(sortedFromNewest) {
-        profilePostsContainer.innerHTML = "";    
-        templates.renderPostTemplates( sortedFromNewest, profilePostsContainer )
-    } else {
-        return;
-    }
-    }
-    function byOldest(a, b) {
-       return new Date(a.created).valueOf() - new Date(b.created).valueOf();
-    };
-    function byNewest(a, b) {
-        return new Date(b.created).valueOf() - new Date(a.created).valueOf();
-     };
 
+    homePostsContainer.innerHTML = "";    
+    templates.renderPostTemplates( sortedFromOldest, homePostsContainer ) 
+    }
+
+export async function sortPostsByNewest(event) {
+    event.preventDefault();
+    const posts = await getPosts();
+    const sortedFromNewest = posts.sort(byNewest);
+        
+    homePostsContainer.innerHTML = "";    
+    templates.renderPostTemplates( sortedFromNewest, homePostsContainer ) 
+    }
+
+    function byOldest(a, b) {
+        const dateA = new Date (a.created);
+        const dateB = new Date (b.created);
+        if (dateA > dateB) return 1;
+        else if (dateA < dateB) return -1;
+        return 0;
+    };
+
+    function byNewest(a, b) {
+        const dateA = new Date (a.created);
+        const dateB = new Date (b.created);
+        if (dateA < dateB) return 1;
+        else if (dateA > dateB) return -1;
+        return 0;
+    }; 
     
-     SortPostsByNewest.addEventListener('click', sortPostsByDate);
-     SortPostsByOldest.addEventListener('click', sortPostsByDate);
+    if (path === '/src/index.html') {
+        SortPostsByOldest.addEventListener('click', sortPostsByOldest);
+        SortPostsByNewest.addEventListener('click', sortPostsByNewest);
+    }
      
 
-    
+   
